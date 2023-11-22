c0a639a3b5faa1270661da71cbb3333a053108db
Двуликий Янус <0xjanus@MacBook-Pro-Dvulikij.local>
2023-11-22 20:18:09 +0500

    save

--- scripts/SkeletonControlling.gd
+++ scripts/SkeletonControlling.gd
@@ -3,42 +3,53 @@ extends CharacterBody2D
 	
 signal timeout
 
-const TIME_PERIOD = 10.0 # 500ms
-var left = false
-var time = 0
-var speed = 25
-var target_position
 @onready var player = $"../Player"
 
+
+const TIME_PERIOD = 3.0 # 10sec
+var speed = 25
 var go = false
+var left = false
+var time = 0
+var last_velocity
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.speed_scale = 1
-	$AnimatedSprite2D.play("idle")
+	idle()
 
 
 func _process(delta):
-	time += delta
-	if time > TIME_PERIOD:
-		left = !left
-		$AnimatedSprite2D.flip_h = left
-		time = 0
-		
+	#time += delta
+	#if time > TIME_PERIOD:
+	#		left = !left
+	#	$AnimatedSprite2D.flip_h = left
+	#	time = 0
+	pass
+
+func idle():
+	$AnimatedSprite2D.speed_scale = 1
+	$AnimatedSprite2D.play("idle")
+
+func walk():
+	$AnimatedSprite2D.speed_scale = 4
+	$AnimatedSprite2D.play("walk")
+
+func attack():
+	$AnimatedSprite2D.play("attack")
+
 func _physics_process(delta):
-	if go:
-		velocity = (player.position - position).normalized() * speed
-		$AnimatedSprite2D.speed_scale = 3
-		$AnimatedSprite2D.play("walk")	
-		move_and_slide()
-
+	if go == true:
+		attack()
+	else:
+		idle()
+		
 	
-
-
-
 func _on_area_2d_body_entered(body):
 	if body.has_method("player"):
-		print("I see you!")
 		go = true
-	else:
+
+
+func _on_area_2d_body_exited(body):
+	if body.has_method("player"):
 		go = false
-	
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -3,14 +3,35 @@ extends CharacterBody2D
 @export var speed = 100
 
 var target = position
+var action
+
 
 func player():
 	pass
-	
-	
+
+# Just wrapper for AnimatedSprite2D object.
+func animation_wrapper(action: String) -> void:
+	$AnimatedSprite2D.play(action)
+
+
+# Wrapper for actions.
+func action_wrapper(action: String) -> void:
+	if action == "run":
+		animation_wrapper(action)
+		# do something...
+	elif action == "idle":
+		animation_wrapper(action)
+		# do something...
+
+
+
+
+# Input handler.
 func _input(event):
 	if event.is_action_pressed("click"):
 		target = get_global_mouse_position()
+		action = "run"
+
 
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
@@ -18,11 +39,10 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.flip_h = true
-	# look_at(target)
-	
+		
 	if position.distance_to(target) > 10:
-		$AnimatedSprite2D.play("run")
+		action_wrapper(action)
 		move_and_slide()
 	else:
-		$AnimatedSprite2D.play("idle")
+		animation_wrapper("idle")
 		
